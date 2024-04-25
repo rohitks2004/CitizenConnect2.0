3c74759240d06a8be17002487fbb9d526b5ae91e
rohitks2004 <ksrohit25@gmail.com>
2024-04-25 15:51:54 +0530

    solved styling

--- client/src/pages/Solved.jsx
+++ client/src/pages/Solved.jsx
@@ -1,13 +1,30 @@
 // Solved.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { RiHomeSmileFill } from "react-icons/ri";
+import "./CSS/Complaints.css";
 
 function Solved() {
   return (
-    <>
-      <h2>Solved</h2>
-      <Link to="/home-Admin">Back to Home Admin</Link>
-    </>
+    <div className='container home-container'>
+        <div className="head1">
+           <h2 className='h2t'>Solved</h2>
+           <Link to="/home-Admin"><RiHomeSmileFill className='home-icon' /></Link>
+        </div>
+        <div className="complaints-container">
+              {/* {solved.map((complaint) => (
+                  <div key={complaint._id} className="complaint">
+                      <h3>{complaint.problem}</h3>
+                      <p><span>Department:</span> {complaint.department}</p>  
+                      <p><span>Description:</span> {complaint.description}</p>
+                      <p><span>Location:</span> {complaint.location}</p>
+                      <p><span>Date:</span> {new Date(complaint.date).toLocaleDateString()}</p>
+                      {complaint.imageUrl && <img src={complaint.imageUrl} alt="Complaint" />}
+                      <button onClick={onResolve} >Resolve</button>
+                  </div>
+              ))} */}
+          </div>
+    </div>
   );
 }
 
